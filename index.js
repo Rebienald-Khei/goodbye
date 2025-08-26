77fee57f83b72b9c0b2a575b217664d38b14f41b
Rebienald-Khei <rebiekheicarpio@gmail.com>
2025-08-27 02:36:55 +0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,44 @@
+const { Client, GatewayIntentBits } = require('discord.js');
+
+const client = new Client({
+    intents: [
+        GatewayIntentBits.Guilds,
+        GatewayIntentBits.GuildMessages,
+        GatewayIntentBits.MessageContent,
+    ]
+});
+
+// USE THE CHANNEL ID YOU GOT FROM !getid
+const GOODBYE_CHANNEL_ID = '1292599101392687134';
+
+client.once('ready', () => {
+    console.log('✅ Bot is online and ready!');
+});
+
+client.on('messageCreate', message => {
+    if (message.author.bot) return;
+
+    if (message.content === '!ping') {
+        message.reply('Pong! 🏓');
+    }
+    
+    if (message.content === '!testgoodbye') {
+        // Only try ONE method to find the channel
+        const channel = client.channels.cache.get(GOODBYE_CHANNEL_ID);
+        
+        if (channel) {
+            // Send message to the goodbye channel
+            channel.send(`😢 Goodbye, **${message.author.tag}**! This is a test! 👋`);
+            // Reply to the user who typed the command
+            message.reply('✅ Goodbye message sent to the goodbye channel!');
+        } else {
+            message.reply('❌ Could not find the goodbye channel! Use !getid to find the correct ID.');
+        }
+    }
+    
+    if (message.content === '!getid') {
+        message.reply(`📝 This channel ID: **${message.channel.id}**`);
+    }
+});
+
+client.login('MTQwOTk1MjgwODE1MjcyNzYxMw.Gh4fWX.uYPy3GwfypiJvI10x4izgPuAVn3r8rEDs_izp4');
